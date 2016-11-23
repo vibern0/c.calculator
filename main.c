8fa57947895c2025b70c24ddad67d2953a070e1d
Bernardo Vieira <bernardo5@live.com.pt>
2016-11-23 00:36:57 +0000

    entrada

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
+	char entrada[32];
+
+	printf("Escreva uma operação:");
+	scanf(entrada, "%s");
+	while(!strcmp(entrada, "sair"))
+	{
+		
+		//funcao interpretar
+		//funcao realizar calculo
+		//imprimir resultado
+		printf("Escreva uma nova operação:");
+		scanf(entrada, "%s");
+	}
 	return 0;
 }

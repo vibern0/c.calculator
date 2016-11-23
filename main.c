87b9ebb493fcdd7f771df7fd0f9df6ad0f6cde5e
Bernardo Vieira <bernardo5@live.com.pt>
2016-11-23 01:14:34 +0000

    calculando

--- main.c
+++ main.c
@@ -9,7 +9,14 @@ void interpretarCalculo(char entrada[], float saida[])
 	token = strtok(entrada, " ");
 	while(token != NULL) 
 	{
-		saida[ordem++] = atof(token);
+		if(ordem == 1)
+			saida[ordem++] = (!strcmp(token, "+") ? (1.f) : (
+								(!strcmp(token, "-") ? (2.f) : (
+									(!strcmp(token, "*") ? (3.f) : (
+										(!strcmp(token, "/") ? (4.f) : (0.f))))))));
+		else
+			saida[ordem++] = atof(token);
+
 		token = strtok(NULL, " ");
 	}
 }
@@ -17,7 +24,21 @@ void interpretarCalculo(char entrada[], float saida[])
 float calcular(float valor1, float valor2, float operacao)
 {
 	float resultado = 0.f;
-	//
+	switch((int)(operacao))
+	{
+		case 1:
+			resultado = valor1 + valor2;
+			break;
+		case 2:
+			resultado = valor1 - valor2;
+			break;
+		case 3:
+			resultado = valor1 * valor2;
+			break;
+		case 4:
+			resultado = valor1 / valor2;
+			break;
+	}
 	return resultado;
 }
 

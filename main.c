87ad938a663f7229bf6ce12d6a8b1389709708f8
Bernardo Vieira <bernardo5@live.com.pt>
2016-11-23 01:03:35 +0000

    calcular

--- main.c
+++ main.c
@@ -14,18 +14,27 @@ void interpretarCalculo(char entrada[], float saida[])
 	}
 }
 
+float calcular(float valor1, float valor2, float operacao)
+{
+	float resultado = 0.f;
+	//
+	return resultado;
+}
+
 int main()
 {
 	char entrada[32];
+	float saida[3];
+	float resultado;
 
 	printf("Escreva uma operação:");
 	scanf(entrada, "%s");
 	while(!strcmp(entrada, "sair"))
 	{
 		
-		//funcao interpretar
-		//funcao realizar calculo
-		//imprimir resultado
+		interpretarCalculo(entrada, saida);
+		resultado = calcular(saida[0], saida[2], saida[1]);
+		printf("Resultado : %.2f", resultado);
 		printf("Escreva uma nova operação:");
 		scanf(entrada, "%s");
 	}
